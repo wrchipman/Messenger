19bbcaebe6ff952635dcedc76dc4cbfd208610f4
wrchipman <wrchipman@gmail.com>
2016-08-24 19:54:50 -0400

    third commit

---
+++ app/src/main/java/com/chipmanservice/messenger/CreateMessageActivity.java
@@ -0,0 +1,26 @@
+package com.chipmanservice.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        startActivity(intent);
+    }
+}
